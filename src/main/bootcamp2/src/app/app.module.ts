f34c049498a9cd9151372cf38782e91af5aa2bcf
sedan7001 <sedan7001@eediom.com>
2019-11-18 15:59:34 +0900

    앱모듈 추가.

--- src/main/bootcamp2/src/app/app.module.ts
+++ src/main/bootcamp2/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceModule, QueryService } from 'eediom-sdk';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		ServiceModule.forRoot({
+			productName: 'Araqne'
+		}),
+		FormsModule
+	],
+	providers: [QueryService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
